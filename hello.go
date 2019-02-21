d5b03e5be1e9b59f038949134dfcd52f5dbb3c04
Anatolii Marandiuc <shamaneeel@mail.ru>
2019-02-21 03:03:30 +0300

    Простейший пример + чтение из файла

---
+++ hello.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	page := ReadFile("Testpage.html")
+	log.Print(page)
+	PORT := ":8080"
+	log.Print("Running server on "+ PORT)
+	http.HandleFunc("/",CompleteTaskFunc)
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func CompleteTaskFunc(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(r.URL.Path))
+}
+
+func ReadFile(path string) string{
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	input := ""
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input+=scanner.Text();
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+    return input
+}
