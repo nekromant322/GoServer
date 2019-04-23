ada75663fd408d10a0e8977997f89e1589a69c4e
Danis <danis-en98@outlook.com>
2019-04-23 21:29:31 +0300

    Автокорректировка стиля

--- db/utils.go
+++ db/utils.go
@@ -27,34 +27,34 @@ func (db Database) query(q string, args ...interface{}) (rows *sql.Rows) {
 	}
 	return rows
 }
-func GetHash(login string, password string) string{
+func GetHash(login string, password string) string {
 
-	key := 1234567;
-	sol := login[0:4];
+	key := 1234567
+	sol := login[0:4]
 
-	hash := password + sol;
-	arr := make([] int, len(hash));
+	hash := password + sol
+	arr := make([]int, len(hash))
 	for i := 0; i < len(hash); i++ {
-		arr[i] = int(hash[i]);
+		arr[i] = int(hash[i])
 	}
 	for i := 0; i < len(arr); i++ {
 		for j := 0; j < key; j++ {
-			arr[i] = arr[i] * arr[j%len(arr)];
-			arr[i] = arr[i]%key;
+			arr[i] = arr[i] * arr[j%len(arr)]
+			arr[i] = arr[i] % key
 		}
-		if(arr[i] % 5 == 0 || arr[i] %11 ==0 || arr[i] %7 ==0 || arr[i] % 13 ==0){
-			arr[i] = 48 + arr[i] % 10;
+		if arr[i]%5 == 0 || arr[i]%11 == 0 || arr[i]%7 == 0 || arr[i]%13 == 0 {
+			arr[i] = 48 + arr[i]%10
 		} else {
 
-			arr[i] = 65 + arr[i] % 26;
+			arr[i] = 65 + arr[i]%26
 		}
 
 	}
-	hash = "";
+	hash = ""
 	for i := 0; i < len(arr); i++ {
-		character := string(arr[i]);
-		hash =hash + character;
+		character := string(arr[i])
+		hash = hash + character
 	}
 
-	return hash;
+	return hash
 }
